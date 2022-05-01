f1e3c9480e1424ab518a0018b1aee5edfef154bb
Ryota Konabe <rt.konabe@gmail.com>
2022-05-01 17:32:01 +0900

    pinNumbersの順序が変になってしまっていたので、修正

--- bowling/pins/numbers/pinNumbers.go
+++ bowling/pins/numbers/pinNumbers.go
@@ -1,21 +1,13 @@
 package pinNumbers
 
-import "bowlingSystem/util"
-
 //PinNumbers ピンの番号を指定する場合に使用します。不正なデータの場合はnilが返却されます。
 type PinNumbers struct {
 	Values []int
 }
 
 func New(numbers ...int) PinNumbers {
-	uniqueNumbers := util.Unique(numbers)
-	if !isValidNumbers(uniqueNumbers) {
-		return PinNumbers{
-			Values: []int{},
-		}
-	}
 	return PinNumbers{
-		Values: uniqueNumbers,
+		Values: makeUniqueAndValidNumbers(numbers),
 	}
 }
 
@@ -23,15 +15,18 @@ func (pinNumbers PinNumbers) Len() int {
 	return len(pinNumbers.Values)
 }
 
-func availableNumbers() []int {
-	return []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-}
-
-func isValidNumbers(numbers []int) bool {
+func makeUniqueAndValidNumbers(numbers []int) []int {
+	bucket := [10]bool{}
+	newNumbers := []int{}
 	for _, v := range numbers {
-		if !util.Contains(availableNumbers(), v) {
-			return false
+		if 1 <= v && v <= 10 {
+			bucket[v-1] = true
 		}
 	}
-	return true
+	for i, v := range bucket {
+		if v {
+			newNumbers = append(newNumbers, i+1)
+		}
+	}
+	return newNumbers
 }
--- bowling/pins/numbers/pinNumbers_test.go
+++ bowling/pins/numbers/pinNumbers_test.go
@@ -10,35 +10,43 @@ func TestNew(t *testing.T) {
 	cases := []struct {
 		numbers []int
 		values  []int
+		len     int
 	}{
 		{
 			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+			10,
 		},
 		{
 			[]int{1},
 			[]int{1},
+			1,
 		},
 		{
 			[]int{},
 			[]int{},
+			0,
 		},
 		{
 			nil,
-			nil,
+			[]int{},
+			0,
 		},
 		{
 			[]int{1, 1, 2, 2, 3, 3, 3, 3},
 			[]int{1, 2, 3},
+			3,
 		},
 		{
 			[]int{1, 2, 3, 11},
-			nil,
+			[]int{1, 2, 3},
+			3,
 		},
 	}
 	for i, c := range cases {
 		failedMessage := fmt.Sprintf("case %d is failed", i)
 		pn := New(c.numbers...)
-		assert.ElementsMatch(t, c.values, pn.Values, failedMessage)
+		assert.Equal(t, c.values, pn.Values, failedMessage)
+		assert.Equal(t, c.len, pn.Len())
 	}
 }
